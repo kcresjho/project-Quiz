6ebc07ab6f331c3038ec0e80bbb2d7a690cf9a73
kcresjho <kc.res@hotmail.com>
2015-07-20 08:50:40 +0200

    cambios presentación p2p Mod06

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,8 @@ exports.answer = function(req, res) {
 		res.render('quizes/answer', {respuesta: 'Incorrecta'});
 	}
 };
+
+// GET /author
+exports.author = function(req, res) {
+	res.render('author');
+};
