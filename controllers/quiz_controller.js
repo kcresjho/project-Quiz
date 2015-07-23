e2d78725cf036981077333081b92f1dd320f557b
kcresjho <kc.res@hotmail.com>
2015-07-23 22:28:44 +0200

    Creamos preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,6 +12,15 @@ exports.load = function(req, res, next, quizId) {
 	).catch(function(error) { next(error);});
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(    // crea objeto quiz
+		{ pregunta: "pregunta", respuesta: "Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
 // GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes) {
@@ -33,4 +42,14 @@ exports.answer = function(req, res) {
 	} 
 	res.render('quizes/answer', 
 			{ quiz: req.quiz, respuesta: resultado});
+};
+
+// POST /quizes/create
+exports.create  = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz );
+
+	// Guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({ fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) // Redirección http (URL relativo) lista de preguntas
 };
